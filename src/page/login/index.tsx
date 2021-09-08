fbdfc30be008a27829fcdc6edcd2dd562562109a
humin <1106805538@qq.com>
2021-09-08 13:26:04 +0800

    before install scss

--- src/page/login/index.tsx
+++ src/page/login/index.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Login = () => (
   <div className="page-login">
-    <div className="banner">adadsa</div>
+    <div className="head">
+      <span>密码登录</span>
+    </div>
+    <div className="banner">
+      <img src="/static/image/login-banner.jpeg" alt="加载失败" />
+    </div>
     <div className="login-form" />
     <div className="notify" />
   </div>
